53a7175043048cf2340ecd60c3ca175f6640e0c9
ChoiYeonho0903 <903yh@naver.com>
2022-02-05 22:04:17 +0900

    Comment 기능

--- Simple-Blog-Project-Share/src/main/java/com/codepresso/blog/controller/CommentController.java
+++ Simple-Blog-Project-Share/src/main/java/com/codepresso/blog/controller/CommentController.java
@@ -1,5 +1,29 @@
 package com.codepresso.blog.controller;
 
+import com.codepresso.blog.service.CommentService;
+import com.codepresso.blog.vo.Result;
+import com.codepresso.blog.vo.UserComment;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class CommentController {
 
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/comment")
+    public Result addComment(@RequestBody UserComment userComment) {
+        commentService.addComment(userComment);
+        return new Result(200, "Success");
+    }
+
+    @GetMapping("/comments")
+    public List<UserComment> userCommentList(@RequestParam("post_id") Integer postId) {
+        return commentService.getCommentList(postId);
+    }
 }
